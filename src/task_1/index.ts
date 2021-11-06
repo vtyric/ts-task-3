6c6a09fbb1bdbafd25a5cb9e6e8e4d820a33d2c3
makar <misha.torno@gmail.com>
2021-11-06 16:08:04 +0500

    решил первый таск

--- src/task_1/index.ts
+++ src/task_1/index.ts
@@ -7,10 +7,48 @@
  * Example new Currency("DOGE", 12.5, "satoshi")
  */
 
-export class Currency{
+export class Currency {
+    private readonly _name: string;
+    private readonly _currencyType: CurrencyType;
+    private readonly _unit: string;
+    private _value: number;
 
+    constructor(name: keyof typeof CurrencyType, value: number, unit: string) {
+        if (!name || value < 0 || value === undefined || !unit) {
+            throw new Error("некорректно введены параметры констркутора");
+        }
+        this._name = name;
+        this._value = value;
+        this._unit = unit;
+        this._currencyType = CurrencyType[name];
+    }
+
+    get name() {
+        return this._name;
+    }
+
+    get currencyType() {
+        return this._currencyType;
+    }
+
+    get unit() {
+        return this._unit;
+    }
+
+    get value() {
+        return this._value;
+    }
+
+    set value(value: number) {
+        if (value < 0) {
+            throw new Error("значение не может быть отрицательным");
+        }
+        this._value = value;
+    }
 }
 
 export enum CurrencyType {
-
+    "ru" = "Мaterial",
+    "bitcoin" = "Сryptocurrency",
+    "alpha" = "МetalDeposit"
 }
